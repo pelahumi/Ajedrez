b1d521cde9a3e9eafcd0f3a053f14590806d13d1
Pelayo <phuermij@myuax.com>
2021-12-11 22:44:04 +0100

    Bucle para jugar

--- Ajedrez.py
+++ Ajedrez.py
@@ -1,3 +1,11 @@
 import chess
 tablero = chess.Board()
 print(tablero)
+
+while tablero.is_checkmate() == False:
+    print(tablero.legal_moves)#Esto mostrará al usuario los posibles movimientos que puede hacer
+    move = input("Introduce tu movimiento: ")
+    tablero.push_san(move)
+    print(tablero)
+    tablero.is_checkmate()
+    
