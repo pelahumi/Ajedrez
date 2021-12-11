488e8ef1cf2343b3c76f14f99d3edd9269e1c323
Pelayo <phuermij@myuax.com>
2021-12-11 22:57:18 +0100

    fichas negras en unicode

--- Ajedrez.py
+++ Ajedrez.py
@@ -1,4 +1,16 @@
 import chess
+
+fichas_negras = {
+    chr(0x265f): "p",
+    chr(0x265c): "r",
+    chr(0x265e): "n",
+    chr(0x256d): "b",
+    chr(0x265b): "q",
+    chr(0x265a): "k"  
+}
+
+
+
 tablero = chess.Board()
 print(tablero)
 
